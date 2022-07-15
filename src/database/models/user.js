712aa0412070bdc501e1e1e2b59b214aa8456302
Pedro Tavares <pedrodc1236@gmail.com>
2022-07-15 17:15:39 -0300

    requisito 02 completo

---
+++ src/database/models/user.js
@@ -0,0 +1,27 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },{
+    timestamps: false,
+    tableName: 'Users',
+    underscored: true,
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'userId', as: 'blogposts'
+    });
+  };
+
+  return User;
+};
+
+module.exports = User;
